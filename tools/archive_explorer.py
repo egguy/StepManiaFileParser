e90f455d6da9e90f79f6ce001a52c560ce7893ff
egguy <crazypops@gmail.com>
2017-09-22 10:05:06 +0200

    Add a tool to explore zip file and parse them without decompressing

---
+++ tools/archive_explorer.py
@@ -0,0 +1,21 @@
+import zipfile
+import sys
+
+from sm_parser.parser import SmParser
+
+filename = sys.argv[1]
+
+zipfile = zipfile.ZipFile(filename)
+
+
+def filter_sm(filename): return filename.lower().endswith(".sm")
+
+
+sm_files = filter(filter_sm, zipfile.namelist())
+
+for i in sm_files:
+    print(i)
+    data = zipfile.open(i)
+    parser = SmParser(data.read())
+    parser.parse()
+    print(parser.song)
