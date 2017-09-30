2f83d40fe3d191cd0a7b312aff861e216fa17300
Etienne G <crazypops@gmail.com>
2017-09-30 14:15:55 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='SmParser',
+      version='0.1',
+      description='A stepmania file format parser',
+      author='Etienne Guillluy',
+      author_email='etienne.guilluy@gmail.com',
+      url='https://github.com/egguy/StepManiaFileParser',
+      packages=find_packages(),
+     )
